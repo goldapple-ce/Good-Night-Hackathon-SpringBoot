213572dda908f76ef1d48b82ab5b9cbf215c04e9
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-02-25 23:51:36 +0900

    init

---
+++ goodnignt/src/main/java/com/example/goodnignt/GoodnigntApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodnignt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodnigntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodnigntApplication.class, args);
+	}
+
+}
---
+++ goodnignt/src/test/java/com/example/goodnignt/GoodnigntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodnignt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodnigntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
